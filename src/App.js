4ed74df899ac5f7eb55f701a40aa8e01d0004b46
umeshkhatiwada13 <umeshkhatiwada13@gmail.com>
2022-11-09 09:19:01 +0545

    feat: addition of responsive css code

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Home from './components/Home';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 import Services from './components/Services';
-import "./styles/App.scss";
-import "./styles/Header.scss";
+import "./styles/app.scss";
+import "./styles/header.scss";
 import "./styles/home.scss";
 import "./styles/footer.scss";
 import "./styles/contact.scss";
+import "./styles/mediaquery.scss";
 function App() {
   return (
     <Router>
